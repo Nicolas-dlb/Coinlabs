efc93007aa935950d162a63ee7d0abe0a17a5aa5
Nicolas-dlb <nicolas_db@outlook.fr>
2021-09-14 17:16:09 +0200

    Add date to tooltip in graph

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -47,9 +47,9 @@ function Graph({
   const CustomTooltip = ({ active, payload, num }: any) => {
     const a = new Date(payload[0]?.payload.timestamp);
 
-    let angle;
+    // let angle;
     if (payload) {
-      angle = payload[0]?.value > 0 ? "140deg" : "215deg";
+      // angle = payload[0]?.value > 0 ? "140deg" : "215deg";
     }
     const divise =
       num === 0
@@ -81,14 +81,14 @@ function Graph({
                   : "XRP"
                 : "Balance"}
             </p>
-            <svg
+            {/* <svg
               style={{ transform: `rotate(${angle})` }}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 492 492"
               fill="#fff"
             >
               <path d="M464.3 207.4l0.8 0.2H135.9l103.5-103.7c5.1-5.1 7.8-11.9 7.8-19.1 0-7.2-2.8-14-7.8-19.1L223.3 49.5c-5.1-5.1-11.8-7.9-19-7.9 -7.2 0-14 2.8-19 7.8L7.8 226.9C2.8 232 0 238.8 0 246c0 7.2 2.8 14 7.8 19.1l177.4 177.4c5.1 5.1 11.8 7.8 19 7.8 7.2 0 13.9-2.8 19-7.8l16.1-16.1c5.1-5.1 7.8-11.8 7.8-19 0-7.2-2.8-13.6-7.8-18.7L134.7 284.4h330c14.8 0 27.3-12.8 27.3-27.6v-22.8C492 219.2 479.2 207.4 464.3 207.4z" />
-            </svg>
+            </svg> */}
             <p style={{ marginLeft: "6px" }}>
               {`${a.getDate()}/${a.getMonth() + 1}`}
             </p>
